096e7f85ea832f01116a8f60aae24408440aafbf
Luckysinghmourya01 <luckysinghmoury2@gmail.com>
2026-04-13 11:56:43 +0530

    done

--- Hibernate-basic/src/com/rays/user/TestUserModel.java
+++ Hibernate-basic/src/com/rays/user/TestUserModel.java
@@ -2,7 +2,10 @@ package com.rays.user;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.List;
 
+import com.rays.crud.TestDelete;
 import com.rays.exception.DublicateRecordException;
 
 public class TestUserModel {
@@ -12,7 +15,63 @@ public class TestUserModel {
 		// testAdd();
 		// testFindByLogin();
 		// testAuthenticate();
-		testAuthenticate1();
+		// testAuthenticate1();
+		// testUpdate();
+		// TestDelete();
+		testSearch();
+	}
+
+	private static void testSearch() {
+
+		UserDTO dto = new UserDTO();
+		UserModel model = new UserModel();
+		
+           dto.setLastName("s");
+		List<UserDTO> list = model.search(dto, 1, 10);
+
+		Iterator<UserDTO> it = list.iterator();
+		while (it.hasNext()) {
+
+			dto = it.next();
+			
+			System.out.print("\t" + dto.getId());
+			System.out.print("\t" + dto.getFirstName());
+			System.out.print("\t" + dto.getLastName());
+			System.out.print("\t" + dto.getLoginId());
+			System.out.print("\t" + dto.getAddress());
+			System.out.println("\t" + dto.getDob());
+			
+		}
+
+	}
+
+	private static void TestDelete() {
+
+		UserDTO dto = new UserDTO();
+
+		dto.setId(1);
+
+		UserModel model = new UserModel();
+		model.delete(dto);
+
+	}
+
+	private static void testUpdate() throws ParseException, DublicateRecordException {
+		UserModel model = new UserModel();
+		UserDTO dto = new UserDTO();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		dto.setId(2);
+		dto.setFirstName("amit");
+		dto.setLastName("mehra");
+		dto.setAddress("bhopal");
+		dto.setLoginId("manthan66@gmail.com");
+		dto.setPassword("amit@123");
+		dto.setDob(sdf.parse("2006-11-25"));
+
+		model.update(dto);
+
 	}
 
 	private static void testAuthenticate1() {
@@ -80,9 +139,9 @@ public class TestUserModel {
 		UserDTO dto = new UserDTO();
 		UserModel model = new UserModel();
 
-		dto.setFirstName("aman");
+		dto.setFirstName("manthan");
 		dto.setLastName("singh");
-		dto.setLoginId("aman66@gmail.com");
+		dto.setLoginId("manthan66@gmail.com");
 		dto.setPassword("man11than22");
 		dto.setAddress("mhow");
 		dto.setDob(sdf.parse("2005-1-21"));
--- Hibernate-basic/src/com/rays/user/UserModel.java
+++ Hibernate-basic/src/com/rays/user/UserModel.java
@@ -1,5 +1,6 @@
 package com.rays.user;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
@@ -35,6 +36,45 @@ public class UserModel {
 		session.close();
 
 		return dto.getId();
+
+	}
+
+	public void update(UserDTO dto) throws DublicateRecordException {
+
+		UserDTO existDto = findByLogin(dto.getLoginId());
+
+		if (existDto != null && !(existDto.getId() == dto.getId())) {
+
+			throw new DublicateRecordException("Login id already exist");
+		}
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.update(dto);
+
+		tx.commit();
+
+		session.close();
+
+	}
+
+	public void delete(UserDTO dto) {
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.delete(dto);
+
+		tx.commit();
+
+		session.close();
 
 	}
 
@@ -82,7 +122,7 @@ public class UserModel {
 		}
 		return dto;
 	}
-	
+
 	public UserDTO authenticate1(String login, String password) {
 
 		UserDTO dto = null;
@@ -91,10 +131,10 @@ public class UserModel {
 
 		Session session = sf.openSession();
 
-		 Query q = session.createQuery("from UserDTO where loginId=? and Password=?");
-		 
-		 q.setString(0, login);
-		 q.setString(1, password);
+		Query q = session.createQuery("from UserDTO where loginId=? and Password=?");
+
+		q.setString(0, login);
+		q.setString(1, password);
 
 		List<UserDTO> list = q.list();
 
@@ -104,5 +144,45 @@ public class UserModel {
 			dto = list.get(0);
 		}
 		return dto;
+	}
+
+	public List<UserDTO> search(UserDTO dto, int pageNo, int pageSize) {
+
+		List<UserDTO> list = new ArrayList<UserDTO>();
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		/* select * from st user where 1=1 */
+		Criteria criteria = session.createCriteria(UserDTO.class);
+
+		if (dto != null) {
+			if (dto.getFirstName() != null && dto.getFirstName().length() > 0) {
+				criteria.add(Restrictions.like("firstName", dto.getFirstName() + "%"));
+			}
+			if(dto.getLastName() != null && dto.getLastName().length() >0) {
+				criteria.add(Restrictions.like("lastName", dto.getLastName() + "%"));
+			}
+			if(dto.getPassword() != null && dto.getPassword().length() >0) {
+				criteria.add(Restrictions.eq("password", dto.getPassword() ));
+			}
+			if(dto.getLoginId() != null && dto.getLoginId().length() >0) {
+				criteria.add(Restrictions.like("loginId", dto.getLoginId()  ));
+			}
+		}
+		
+		if(pageNo > 0) {
+			pageNo = (pageNo - 1) * pageSize;
+			criteria.setFirstResult(pageNo);
+			criteria.setMaxResults(pageSize);
+			
+		list = 	criteria.list();
+		
+		
+		}
+		
+		return list;
+
 	}
 }
